c2d6958976cd65599cdabc0a01998e4cc458955e
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-02-16 20:37:34 -0700

    End of Chapter 7

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Ruth Torres Castillo on 2/16/18.
+//  Copyright © 2018 New Mexico State University. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye",
+                                     withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html",
+                             textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
